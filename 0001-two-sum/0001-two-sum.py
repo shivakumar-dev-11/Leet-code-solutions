a346592b75bb7cc4c3b451efb30affa815c3ae5f
shiva kumar <bashamonishivakumar081@gmail.com>
2026-09-10 15:32:26 +0530

    Time: 2092 ms (37.68%), Space: 13.3 MB (36.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,4 +5,4 @@ class Solution(object):
             for j in range(i + 1, n):
                 if nums[i] + nums[j] == target:
                     return [i, j]
-        return []  # No solution found
+        return []
