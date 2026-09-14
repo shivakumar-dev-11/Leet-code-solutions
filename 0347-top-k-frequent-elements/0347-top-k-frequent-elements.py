3a36bf7455c16bd202edbca742cf135e49ddb90b
shiva kumar <bashamonishivakumar081@gmail.com>
2026-09-14 22:16:28 +0530

    Time: 4 ms (93.04%), Space: 13.9 MB (96.88%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -11,23 +11,6 @@ class Solution(object):
             else:
                 freq[num] = 1
 
-        answer = []
+        sorted_num = sorted(freq, key = freq.get, reverse = True )
 
-       
-        for i in range(k):
-
-            highest = 0
-            highest_num = None
-
-            for num in freq:
-
-                if freq[num] > highest:
-                    highest = freq[num]
-                    highest_num = num
-
-            answer.append(highest_num)
-
-           
-            del freq[highest_num]
-
-        return answer
+        return sorted_num [:k]
