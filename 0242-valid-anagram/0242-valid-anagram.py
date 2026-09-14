87dc3674617dbc561cef56f7870b0636db40a90b
shiva kumar <bashamonishivakumar081@gmail.com>
2026-09-14 22:43:19 +0530

    Time: 11 ms (95.97%), Space: 12.4 MB (86.47%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s)!=len(t):
+            return False
+
+    
+        freq = {}
+        for i in s :
+            if i in freq:
+                freq[i]+=1
+            else:
+                freq[i]=1
+        for i in t :
+            if i in freq:
+                freq[i] -=1
+            else:
+                return False
+        for i in freq:
+            if freq[i] != 0:
+                return False
+
+        return True      
+
+
+            
+
+
+
+
+       
+    
+
+       
+      
+
+            
